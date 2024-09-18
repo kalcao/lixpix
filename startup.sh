b790e8c022c5626fe173c5fa02bf728c0484dbfc
kalcao <lnstagram@kakao.com>
2024-09-18 14:39:23 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,15 +4,30 @@ apt-get update && apt-get install -y openssh-server sudo
 
 # Set root password
 echo "root:${SSH_PASSWORD}" | chpasswd
-
-# Start SSH service
-service ssh start
-
-# Ensure systemd is set up correctly
+case $OS_VERSION in
+    "ubuntu2004")
+        image="ubuntu:20.04"
+        ;;
+    "debian10")
+        image="debian:10"
+        ;;
+    "alpine")
+        image="alpine"
+        ;;
+    *)
+        echo "Unsupported OS version. Exiting..."
+        exit 1
+        ;;
+esac
 if [[ "$OS_VERSION" == "ubuntu2004" || "$OS_VERSION" == "debian10" ]]; then
   apt-get install -y systemd systemd-sysv
   service systemd start
 fi
 
-# Keep container running
+# Ensure SSH service is running
+service ssh start
+
+# Run the selected OS
+exec /sbin/init
+
 tail -f /dev/null
